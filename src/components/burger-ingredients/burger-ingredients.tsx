7d03149f26625eea8698f2f9cfcea49c28ff13de
Sergei Shamardin <sergei74660@gmail.com>
2026-02-28 03:36:51 +0300

    Завершена работа над проектом спринта: добавлены тесты и Cypress

--- src/components/burger-ingredients/burger-ingredients.tsx
+++ src/components/burger-ingredients/burger-ingredients.tsx
@@ -2,7 +2,7 @@ import { useState, useRef, useEffect, FC } from 'react';
 import { useInView } from 'react-intersection-observer';
 
 import { TTabMode } from '@utils-types';
-import { BurgerIngredientsUI } from '../ui/burger-ingredients';
+import { BurgerIngredientsUI } from '@ui';
 import { useSelector } from '../../services/store';
 import {
   getBuns,
